085932a0a83cef8171ebedaae9d2c84561e49af8
Rustam Shorov <srmparusa@gmail.com>
2019-01-30 15:58:46 +0300

    3. Fixed the func for fetching data from google. Added loading indicator

--- qulixTestTask/Controller/CustomSearchViewController.swift
+++ qulixTestTask/Controller/CustomSearchViewController.swift
@@ -12,19 +12,43 @@ class CustomSearchViewController: UIViewController {
 
     let searchEngine = CustomSearchProvider()
     
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var searchButtonOutlet: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadingIndicator.isHidden = true
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadTableView), name: NSNotification.Name("load"), object: nil)
     }
 
     @IBAction func searchButtonPressed(_ sender: UIButton) {
+        loadingIndicator.startAnimating()
         if let quary = textField.text {
             searchEngine.fetchLinks(quary: quary)
+            
         }
     }
+    @objc func reloadTableView() {
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.loadingIndicator.stopAnimating()
+        }
+    }
+}
+
+extension CustomSearchViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchEngine.itemsArray.count
+    }
     
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "resultCell", for: indexPath) as? ResultTableViewCell else { return ResultTableViewCell() }
+        
+        cell.configure(titleText: searchEngine.itemsArray[indexPath.row].snippet, linkText: searchEngine.itemsArray[indexPath.row].link)
+        
+        return cell
+    }
 }
 
---
+++ qulixTestTask/View/ResultTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  ResultTableViewCell.swift
+//  qulixTestTask
+//
+//  Created by Rustam Shorov on 30.01.2019.
+//  Copyright © 2019 Rustam Shorov. All rights reserved.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var link: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    func configure(titleText: String, linkText: String) {
+        title.text = titleText
+        link.text = linkText
+    }
+
+}
